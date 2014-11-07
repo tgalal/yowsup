c9f5fa4a6e016e1f870783b200ae0802a53b1bf7
Tarek Galal <tare2.galal@gmail.com>
2014-11-07 03:21:25 +0100

    code clean up in authenticator

--- Yowsup/layers/auth/authenticator.py
+++ Yowsup/layers/auth/authenticator.py
@@ -8,6 +8,7 @@ from protocolentities import *
 class YowAuthenticatorLayer(YowProtocolLayer):
     STATE_AUTHED    = 0
     STATE_NOAUTH    = 1
+
     def __init__(self):
         handleMap = {
             "stream:features": self.handleStreamFeatures,
@@ -17,32 +18,27 @@ class YowAuthenticatorLayer(YowProtocolLayer):
         }
         super(YowAuthenticatorLayer, self).__init__(handleMap)
         self.supportsReceiptAcks = False
-        self.state = YowAuthenticatorLayer.STATE_NOAUTH 
+        self.state = YowAuthenticatorLayer.STATE_NOAUTH
 
-    # def init(self):
-    #     self.sendFeatures()
-    #     self.sendAuth();
-    #     self.initUpper()
-    #     return True
-
-
-    def onEvent(self, event):
-        if event.getName() == "network.state.connected":
-            self.sendFeatures()
-            self.sendAuth();
-
-    def sendFeatures(self):
-        self.entityToLower(StreamFeaturesProtocolEntity())
-
-    def entityToLower(self, protocolentity):
-        self.toLower(protocolentity.toProtocolTreeNode())
-
-    def sendAuth(self):
-        self.entityToLower(AuthProtocolEntity(YowAuthenticatorLayer.getProp("credentials")[0]))
+    def __str__(self):
+        return "Autheticator Layer"
 
     def send(self, data):
         self.entityToLower(data)
 
+    def entityToLower(self, protocolentity):
+        self.toLower(protocolentity.toProtocolTreeNode())
+
+    def onEvent(self, event):
+        if event.getName() == "network.state.connected":
+            self.login()
+
+    ## general methods
+    def login(self):
+        self.__sendFeatures()
+        self.__sendAuth();
+
+    ###recieved node handlers handlers
     def handleStreamFeatures(self, node):
         nodeEntity = StreamFeaturesProtocolEntity.fromProtocolTreeNode(node)
         self.supportsReceiptAcks  = nodeEntity.supportsReceiptAcks()
@@ -55,12 +51,16 @@ class YowAuthenticatorLayer(YowProtocolLayer):
 
     def handleChallenge(self, node):
         nodeEntity = ChallengeProtocolEntity.fromProtocolTreeNode(node)
-        self.sendResponse(nodeEntity.getNonce())
+        self.__sendResponse(nodeEntity.getNonce())
 
-    def processSuccessNode(self, node):
-        self.state = YowAuthenticatorLayer.STATE_AUTHED
+    ##senders
+    def __sendFeatures(self):
+        self.entityToLower(StreamFeaturesProtocolEntity())
 
-    def sendResponse(self,nonce):
+    def __sendAuth(self):
+        self.entityToLower(AuthProtocolEntity(YowAuthenticatorLayer.getProp("credentials")[0]))
+
+    def __sendResponse(self,nonce):
         keys = KeyStream.generateKeys(YowAuthenticatorLayer.getProp("credentials")[1], nonce)
 
         inputKey = KeyStream(keys[2], keys[3])
@@ -80,12 +80,8 @@ class YowAuthenticatorLayer(YowProtocolLayer):
         encoded = outputKey.encodeMessage(nums, 0, 4, len(nums) - 4)
         authBlob = "".join(map(chr, encoded))
 
-
         responseEntity = ResponseProtocolEntity(authBlob)
         self.entityToLower(responseEntity)
         YowCryptLayer.setProp("outputKey", outputKey)
-
-    def __str__(self):
-        return "Autheticator Layer"
 
 
