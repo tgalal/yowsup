90745023b7d06eb035e433ba9630882d470f2fd8
Tarek Galal <tare2.galal@gmail.com>
2014-11-10 21:22:52 +0100

    Fixed test_message_text

--- Yowsup/layers/messages/protocolentities/test_message_text.py
+++ Yowsup/layers/messages/protocolentities/test_message_text.py
@@ -1,8 +1,9 @@
 from .message_text import TextMessageProtocolEntity
 from Yowsup.structs import ProtocolTreeNode
 from Yowsup.structs.protocolentity import ProtocolEntityTest
+from .test_message import MessageProtocolEntityTest
 
-class TextMessageProtocolEntityTest(ProtocolEntityTest):
+class TextMessageProtocolEntityTest(MessageProtocolEntityTest):
     def setUp(self):
         super(TextMessageProtocolEntityTest, self).setUp()
         self.ProtocolEntity = TextMessageProtocolEntity
