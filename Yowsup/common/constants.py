863af3780d4aba55d4ef9fe51e914121d37c4e88
Tarek Galal <tare2.galal@gmail.com>
2014-11-20 22:47:25 +0100

    Updated resource

--- Yowsup/common/constants.py
+++ Yowsup/common/constants.py
@@ -1,6 +1,6 @@
 class YowConstants:
     DOMAIN       = "s.whatsapp.net"
-    RESOURCE     = "S40-2.12.15"
+    RESOURCE     = "S40-2.12.49"
     ENDPOINTS     = (
         ("c2.whatsapp.net", 443),
         )
