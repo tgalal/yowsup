f01e0ea8216c745ff336e2f651c21bf9db902bf3
Tarek Galal <tare2.galal@gmail.com>
2014-11-20 22:49:33 +0100

    Contact notification initial commit
    Added remove contact

--- Yowsup/layers/protocol_notifications/protocolentities/__init__.py
+++ Yowsup/layers/protocol_notifications/protocolentities/__init__.py
@@ -1,4 +1,5 @@
 from .notification                  import NotificationProtocolEntity
 from .notification_picture          import PictureNotificationProtocolEntity
 from .notification_picture_set      import SetPictureNotificationProtocolEntity
+from .notification_contact_remove   import RemoveContactNotificationProtocolEntity
 from .notification_status           import StatusNotificationProtocolEntity
