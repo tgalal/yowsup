94b9065a92f5cd83982d1d1c5068eaf02f8c7979
Tarek Galal <tare2.galal@gmail.com>
2014-11-23 04:52:15 +0100

    Added setup.py

---
+++ src/setup.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from setuptools import setup, find_packages
+import yowsup
+
+setup(
+    name='yowsup',
+    version=yowsup.__version__,
+    url='http://github.com/tgalal/yowsup/',
+    license='MIT License',
+    author='Tarek Galal',
+    tests_require=[],
+    install_requires=['python-dateutil'],
+    #scripts = ['go_foo.py'],
+    #cmdclass={'test': PyTest},
+    author_email='tare2.galal@gmail.com',
+    description='A WhatsApp python library',
+    #long_description=long_description,
+    packages= find_packages(),
+    include_package_data=True,
+    platforms='any',
+    #test_suite='',
+    classifiers = [
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        #'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+        ],
+    #extras_require={
+    #    'testing': ['pytest'],
+    #}
+)
