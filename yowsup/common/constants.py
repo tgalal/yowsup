3527daf676e221a21d24052928881910e2f8efe5
Tarek Galal <tare2.galal@gmail.com>
2014-11-24 02:38:43 +0100

    Added token data to constants

--- yowsup/common/constants.py
+++ yowsup/common/constants.py
@@ -4,3 +4,11 @@ class YowConstants:
     ENDPOINTS     = (
         ("c2.whatsapp.net", 443),
         )
+
+    DATA_CLIENT = {
+        "v": "2.12.49",
+        "r": "S40-2.12.49",
+        "u": "WhatsApp/2.12.49 S40Version/14.26 Device/Nokia302",
+        "t": "PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1413401214298{phone}",
+        "d": "Nokia302"
+    }
