0c1569e665e36d6a7659c04f95363ceb42d8129b
Tarek Galal <tare2.galal@gmail.com>
2014-11-30 17:01:11 +0100

    Updated reg token

--- yowsup/common/constants.py
+++ yowsup/common/constants.py
@@ -1,14 +1,14 @@
 class YowConstants:
     DOMAIN       = "s.whatsapp.net"
-    RESOURCE     = "S40-2.12.49"
+    RESOURCE     = "S40-2.12.53"
     ENDPOINTS     = (
         ("c2.whatsapp.net", 443),
         )
 
     DATA_CLIENT = {
-        "v": "2.12.49",
-        "r": "S40-2.12.49",
-        "u": "WhatsApp/2.12.49 S40Version/14.26 Device/Nokia302",
-        "t": "PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1413401214298{phone}",
+        "v": "2.12.53",
+        "r": "S40-2.12.53",
+        "u": "WhatsApp/2.12.53 S40Version/14.26 Device/Nokia302",
+        "t": "PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1416435341393{phone}",
         "d": "Nokia302"
     }
