4b010dfba56c22ea7159f5bc9f2dce0321e42d09
Tarek Galal <tare2.galal@gmail.com>
2014-12-28 11:52:32 +0100

    Yowsup env initial commit
    S40 and android envs

---
+++ yowsup/env/env_s40.py
@@ -0,0 +1,36 @@
+from .env import YowsupEnv
+import base64
+import hashlib
+class S40YowsupEnv(YowsupEnv):
+    _VERSION = "2.12.53"
+    _OS_NAME= "S40"
+    _OS_VERSION = "14.26"
+    _DEVICE_NAME = "Nokia302"
+    _TOKEN_STRING  = "PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1416435341393{phone}"
+    _AXOLOTL = False
+
+    def getVersion(self):
+        return self.__class__._VERSION
+
+    def getOSName(self):
+        return self.__class__._OS_NAME
+
+    def getOSVersion(self):
+        return self.__class__._OS_VERSION
+
+    def getDeviceName(self):
+        return self.__class__._DEVICE_NAME
+
+    def isAxolotlEnabled(self):
+        return self.__class__._AXOLOTL
+
+    def getToken(self, phoneNumber):
+        return hashlib.md5(self.__class__._TOKEN_STRING.format(phone = phoneNumber).encode()).hexdigest()
+
+    def getUserAgent(self):
+        return self.__class__._USERAGENT_STRING.format(
+            WHATSAPP_VERSION = self.getVersion(),
+            OS_NAME = self.getOSName() + "Version",
+            OS_VERSION = self.getOSVersion(),
+            DEVICE_NAME = self.getDeviceName()
+        )
