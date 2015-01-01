6f5cc9b16126dc280e972d2c3a22609f7de92457
Tarek Galal <tare2.galal@gmail.com>
2015-01-01 15:41:07 +0100

    Added interface for send iq with success/err clbk

--- yowsup/layers/__init__.py
+++ yowsup/layers/__init__.py
@@ -89,15 +89,17 @@ class YowLayer(object):
 
 
 class YowProtocolLayer(YowLayer):
-    def __init__(self, handleMap = {}):
+    def __init__(self, handleMap = None):
         super(YowProtocolLayer, self).__init__()
-        self.handleMap = handleMap
+        self.handleMap = handleMap or {}
+        self.iqRegistry = {}
 
     def receive(self, node):
-        if node.tag in self.handleMap:
-            recv, _ = self.handleMap[node.tag]
-            if recv:
-                recv(node)
+        if not self.processIqRegistry(node):
+            if node.tag in self.handleMap:
+                recv, _ = self.handleMap[node.tag]
+                if recv:
+                    recv(node)
 
     def send(self, entity):
         if entity.getTag() in self.handleMap:
@@ -115,9 +117,30 @@ class YowProtocolLayer(YowLayer):
     def raiseErrorForNode(self, node):
         raise ValueError("Unimplemented notification type %s " % node)
 
+
+    def _sendIq(self, iqEntity, onSuccess = None, onError = None):
+        self.iqRegistry[iqEntity.getId()] = (iqEntity, onSuccess, onError)
+        self.toLower(iqEntity.toProtocolTreeNode())
+
+    def processIqRegistry(self, protocolTreeNode):
+        if protocolTreeNode.tag == "iq":
+            iq_id = protocolTreeNode["id"]
+            if iq_id in self.iqRegistry:
+                originalIq, successClbk, errorClbk = self.iqRegistry[iq_id]
+                del self.iqRegistry[iq_id]
+
+                if protocolTreeNode["type"] == "result" and successClbk:
+                    successClbk(protocolTreeNode, originalIq)
+                elif protocolTreeNode["type"] == "error" and errorClbk:
+                    errorClbk(protocolTreeNode, originalIq)
+                return True
+
+        return False
+
 class YowParallelLayer(YowLayer):
-    def __init__(self, sublayers = []):
+    def __init__(self, sublayers = None):
         super(YowParallelLayer, self).__init__()
+        self.sublayers = sublayers or []
         self.sublayers = tuple([sublayer() for sublayer in sublayers])
         for s in self.sublayers:
             #s.setLayers(self, self)
