e2eb46a29b6f0a2ce950a38c999329dd1dbbb9df
Tarek Galal <tare2.galal@gmail.com>
2015-01-26 08:35:37 +0100

    Stackbuilder to build Echoclient stack

--- yowsup/demos/echoclient/stack.py
+++ yowsup/demos/echoclient/stack.py
@@ -1,49 +1,17 @@
-from yowsup.stacks import YowStack
 from .layer import EchoLayer
 from yowsup.layers import YowLayerEvent
 from yowsup.layers.auth                        import YowCryptLayer, YowAuthenticationProtocolLayer, AuthError
-from yowsup.layers.coder                       import YowCoderLayer
 from yowsup.layers.network                     import YowNetworkLayer
-from yowsup.layers.protocol_messages           import YowMessagesProtocolLayer
-from yowsup.layers.protocol_media              import YowMediaProtocolLayer
-from yowsup.layers.stanzaregulator             import YowStanzaRegulator
-from yowsup.layers.protocol_receipts           import YowReceiptProtocolLayer
-from yowsup.layers.protocol_acks               import YowAckProtocolLayer
-from yowsup.layers.logger                      import YowLoggerLayer
-from yowsup.layers.axolotl                     import YowAxolotlLayer
-from yowsup.common import YowConstants
 from yowsup import env
-
+from yowsup.stacks import YowStackBuilder
 class YowsupEchoStack(object):
     def __init__(self, credentials, encryptionEnabled = False):
-        if encryptionEnabled:
-            env.CURRENT_ENV = env.S40YowsupEnv()
-            layers = (
-                EchoLayer,
-                (YowAuthenticationProtocolLayer, YowMessagesProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer, YowMediaProtocolLayer),
-                YowAxolotlLayer,
-                YowLoggerLayer,
-                YowCoderLayer,
-                YowCryptLayer,
-                YowStanzaRegulator,
-                YowNetworkLayer
-            )
-        else:
-            layers = (
-                EchoLayer,
-                (YowAuthenticationProtocolLayer, YowMessagesProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer, YowMediaProtocolLayer),
-                YowLoggerLayer,
-                YowCoderLayer,
-                YowCryptLayer,
-                YowStanzaRegulator,
-                YowNetworkLayer
-            )
 
-        self.stack = YowStack(layers)
-        self.stack.setProp(YowAuthenticationProtocolLayer.PROP_CREDENTIALS, credentials)
-        self.stack.setProp(YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0])
-        self.stack.setProp(YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN)
-        self.stack.setProp(YowCoderLayer.PROP_RESOURCE, env.CURRENT_ENV.getResource())
+        if not encryptionEnabled:
+            env.CURRENT_ENV = env.S40YowsupEnv()
+
+        self.stack = YowStackBuilder.getDefaultStack(EchoLayer, encryptionEnabled, groups=False, media = True, privacy=False)
+        self.stack.setCredentials(credentials)
 
     def start(self):
         self.stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))
