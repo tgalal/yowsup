8eed1ddc81ee8a72603b85c79cfcdaf278d306cd
Tarek Galal <tare2.galal@gmail.com>
2015-04-18 00:31:09 +0200

    Fixed 675 Wrong license in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     name='yowsup2',
     version=yowsup.__version__,
     url='http://github.com/tgalal/yowsup/',
-    license='MIT License',
+    license='GPL-3+',
     author='Tarek Galal',
     tests_require=[],
     install_requires = deps,
@@ -37,7 +37,7 @@ setup(
         'Natural Language :: English',
         #'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved ::  GPL-3+  License',
         'Operating System :: OS Independent',
         'Topic :: Software Development :: Libraries :: Python Modules'
         ],
