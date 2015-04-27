86e2e482a9c382ab241320dda3d2418ef3673cc6
Antonio Galea <antonio.galea@gmail.com>
2015-04-27 11:19:00 +0200

    get group info v2

--- yowsup/layers/protocol_groups/protocolentities/iq_groups_info.py
+++ yowsup/layers/protocol_groups/protocolentities/iq_groups_info.py
@@ -2,19 +2,23 @@ from yowsup.structs import ProtocolEntity, ProtocolTreeNode
 from .iq_groups import GroupsIqProtocolEntity
 class InfoGroupsIqProtocolEntity(GroupsIqProtocolEntity):
     '''
-    <iq id="{{id}}"" type="get" to="g.us" xmlns="w:g">
-        <query></query>
+    <iq id="{{id}}"" type="get" to="{{group_jid}}" xmlns="w:g2">
+        <query request="interactive"></query>
     </iq>
 
 
     '''
 
-    def __init__(self):
-        super(InfoGroupsIqProtocolEntity, self).__init__(to = "g.us", _id = _id, _type = "get")
+    def __init__(self, group_jid, _id=None):
+        super(InfoGroupsIqProtocolEntity, self).__init__(to = group_jid, _id = _id, _type = "get", xmlns="w:g2")
+        self.setProps(group_jid)
+
+    def setProps(self, group_jid):
+        self.group_jid = group_jid
 
     def toProtocolTreeNode(self):
         node = super(InfoGroupsIqProtocolEntity, self).toProtocolTreeNode()
-        node.addChild(ProtocolTreeNode("query"))
+        node.addChild(ProtocolTreeNode("query"), {"request": "interactive"})
         return node
 
     @staticmethod
@@ -22,4 +26,5 @@ class InfoGroupsIqProtocolEntity(GroupsIqProtocolEntity):
         assert node.getChild("query") is not None, "Not a goups info iq node %s" % node
         entity = GroupsIqProtocolEntity.fromProtocolTreeNode(node)
         entity.__class__ = InfoGroupsIqProtocolEntity
+        entity.setProps(node.getAttributeValue("to"))
         return entity
