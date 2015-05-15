22e72e09c1b401585d6597a93b1ec0212a18d82a
Cataractar <cataractar@gmail.com>
2015-05-15 07:58:54 +0200

    disable ping thread if interval is zero or less
    The ping thread won't be started if PROP_PING_INTERVAL is zero or negative, to prevent ping spamming and to provide a option to disable it manually

--- yowsup/layers/protocol_iq/layer.py
+++ yowsup/layers/protocol_iq/layer.py
@@ -60,9 +60,10 @@ class YowIqProtocolLayer(YowProtocolLayer):
     def onEvent(self, event):
         name = event.getName()
         if name == YowAuthenticationProtocolLayer.EVENT_AUTHED:
-            if not self._pingThread:
+            interval = self.getProp(self.__class__.PROP_PING_INTERVAL, 50)
+            if not self._pingThread and interval > 0:
                 self._pingQueue = {}
-                self._pingThread = YowPingThread(self, self.getProp(self.__class__.PROP_PING_INTERVAL, 50))
+                self._pingThread = YowPingThread(self, interval)
                 self.__logger.debug("starting ping thread.")
                 self._pingThread.start()
         elif name == YowNetworkLayer.EVENT_STATE_DISCONNECT or name == YowNetworkLayer.EVENT_STATE_DISCONNECTED:
