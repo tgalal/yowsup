16773a51bd2c42335913e733c470bc1cde26f28b
Bram <bram-lbjfhc@mail.wizbit.be>
2015-06-12 22:16:36 +0200

    Handle multiple items in receipt

--- yowsup/layers/protocol_receipts/protocolentities/receipt_incoming.py
+++ yowsup/layers/protocol_receipts/protocolentities/receipt_incoming.py
@@ -17,13 +17,31 @@ class IncomingReceiptProtocolEntity(ReceiptProtocolEntity):
     read by participant in group:
     <receipt participant="xxxxxxxxxx@s.whatsapp.net" t="1431204235" from="yyyyyyyyyyyyy@g.us" id="1431204051-9" type="read"></receipt>
 
+    multiple items:
+    <receipt type="read" from="xxxxxxxxxxxx@s.whatsapp.net" id="1431364583-191" t="1431365553">
+        <list>
+            <item id="1431364572-189"></item>
+            <item id="1431364575-190"></item>
+        </list>
+    </receipt>
+
+    multiple items to group:
+    <receipt participant="xxxxxxxxxxxx@s.whatsapp.net" t="1431330533" from="yyyyyyyyyyyyyy@g.us" id="1431330385-323" type="read">
+        <list>
+            <item id="1431330096-317"></item>
+            <item id="1431330373-320"></item>
+            <item id="1431330373-321"></item>
+            <item id="1431330385-322"></item>
+        </list>
+    </receipt>
+
     INCOMING
     <receipt offline="0" from="xxxxxxxxxx@s.whatsapp.net" id="1415577964-1" t="1415578027"></receipt>
     '''
 
-    def __init__(self, _id, _from, timestamp, offline = None, type = None, participant = None):
+    def __init__(self, _id, _from, timestamp, offline = None, type = None, participant = None, items = None):
         super(IncomingReceiptProtocolEntity, self).__init__(_id)
-        self.setIncomingData(_from, timestamp, offline, type, participant)
+        self.setIncomingData(_from, timestamp, offline, type, participant, items)
 
     def getType(self):
         return self.type
@@ -34,7 +52,7 @@ class IncomingReceiptProtocolEntity(ReceiptProtocolEntity):
     def getFrom(self, full = True):
         return self._from if full else self._from.split('@')[0]
 
-    def setIncomingData(self, _from, timestamp, offline, type = None, participant = None):
+    def setIncomingData(self, _from, timestamp, offline, type = None, participant = None, items = None):
         self._from = _from
         self.timestamp = timestamp
         self.type = type
@@ -43,6 +61,7 @@ class IncomingReceiptProtocolEntity(ReceiptProtocolEntity):
             self.offline = True if offline == "1" else False
         else:
             self.offline = None
+        self.items = items
 
     def toProtocolTreeNode(self):
         node = super(IncomingReceiptProtocolEntity, self).toProtocolTreeNode()
@@ -54,6 +73,16 @@ class IncomingReceiptProtocolEntity(ReceiptProtocolEntity):
             node.setAttribute("type", self.type)
         if self.participant is not None:
             node.setAttribute("participant", self.participant)
+
+        if self.items is not None:
+            inodes = []
+            for item in self.items:
+                inode = ProtocolTreeNode("item", {"id": item})
+                inodes.append(inode)
+
+            lnode = ProtocolTreeNode("list")
+            lnode.addChildren(inodes)
+            node.addChild(lnode)
         return node
 
     def __str__(self):
@@ -66,6 +95,8 @@ class IncomingReceiptProtocolEntity(ReceiptProtocolEntity):
             out += "Type: %s\n" % (self.type)
         if self.participant is not None:
             out += "Participant: %s\n" % (self.participant)
+        if self.items is not None:
+            out += "Items: %s\n" % " ".join(self.items)
         return out
 
     def ack(self):
@@ -73,11 +104,18 @@ class IncomingReceiptProtocolEntity(ReceiptProtocolEntity):
 
     @staticmethod
     def fromProtocolTreeNode(node):
+        items = None
+        listNode = node.getChild("list")
+        if listNode is not None:
+            items = []
+            for inode in listNode.getAllChildren("item"):
+               items.append(inode["id"])
         return IncomingReceiptProtocolEntity(
             node.getAttributeValue("id"),
             node.getAttributeValue("from"),
             node.getAttributeValue("t"),
             node.getAttributeValue("offline"),
             node.getAttributeValue("type"),
-            node.getAttributeValue("participant")
+            node.getAttributeValue("participant"),
+            items
             )
