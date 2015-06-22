85c0de0736a6544272b0f4c53571de30b7c443ae
Nathan A. Ferch <nf@marginal.net>
2015-06-22 14:47:27 -0400

    Add twisted to dependency list

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import yowsup
 import platform
 
-deps = ['python-dateutil', 'argparse', 'python-axolotl>=0.1.7', 'pillow']
+deps = ['python-dateutil', 'argparse', 'python-axolotl>=0.1.7', 'pillow', 'twisted']
 
 if platform.system().lower() == "windows":
     deps.append('pyreadline')
