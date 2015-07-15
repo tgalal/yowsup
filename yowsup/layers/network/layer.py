08d4af43a45590c747c5c73d715c1db6955b8106
Antonio Galea <antonio.galea@gmail.com>
2015-07-15 16:49:26 +0200

    fix: EVENT_STATE_DISCONNECTED is not always received in upper layers

--- yowsup/layers/network/layer.py
+++ yowsup/layers/network/layer.py
@@ -56,7 +56,9 @@ class YowNetworkLayer(YowLayer, asyncore.dispatcher_with_send):
 
     def handle_close(self, reason = "Connection Closed"):
         logger.debug("Disconnected, reason: %s" % reason)
-        self.emitEvent(YowLayerEvent(self.__class__.EVENT_STATE_DISCONNECTED, reason = reason, detached=True))
+        ev = YowLayerEvent(self.__class__.EVENT_STATE_DISCONNECTED, reason = reason, detached=True)
+        self.emitEvent(ev)
+        self.getStack().broadcastEvent(ev)
         self.close()
         
     def handle_error(self):
