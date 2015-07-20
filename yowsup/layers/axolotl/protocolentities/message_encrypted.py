54a0b01987130db2a8472e18a012bec4a4d40c28
Tarek Galal <tare2.galal@gmail.com>
2015-07-20 10:02:37 +0200

    Updated enc stanza example

--- yowsup/layers/axolotl/protocolentities/message_encrypted.py
+++ yowsup/layers/axolotl/protocolentities/message_encrypted.py
@@ -3,8 +3,8 @@ from yowsup.structs import ProtocolTreeNode
 import sys
 class EncryptedMessageProtocolEntity(MessageProtocolEntity):
     '''
-    <message retry="1" from="4917675341470@s.whatsapp.net" t="1418906418" offline="1" type="text" id="1418906377-1" notify="Tarek Galal">
-<enc av="Android/2.11.456" type="{{type}}" v="1">
+    <message retry="1" from="49xxxxxxxx@s.whatsapp.net" t="1418906418" offline="1" type="text" id="1418906377-1" notify="Tarek Galal">
+<enc type="{{type}}" v="{{1 || 2}}">
 HEX:33089eb3c90312210510e0196be72fe65913c6a84e75a54f40a3ee290574d6a23f408df990e718da761a210521f1a3f3d5cb87fde19fadf618d3001b64941715efd3e0f36bba48c23b08c82f2242330a21059b0ce2c4720ec79719ba862ee3cda6d6332746d05689af13aabf43ea1c8d747f100018002210d31cd6ebea79e441c4935f72398c772e2ee21447eb675cfa28b99de8d2013000</enc>
 </message>
     '''
