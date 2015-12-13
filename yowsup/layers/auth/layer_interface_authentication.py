4353d33e96f6ab3d7dfceb157712eb8e7279d26e
Tarek Galal <tare2.galal@gmail.com>
2015-12-13 20:51:23 +0100

    Fixed YowParallelLayer returning layer instead of its interface
    Fixes #1061, can't login in cli via /login

--- yowsup/layers/auth/layer_interface_authentication.py
+++ yowsup/layers/auth/layer_interface_authentication.py
@@ -2,7 +2,7 @@ from yowsup.layers import YowLayerInterface
 
 class YowAuthenticationProtocolLayerInterface(YowLayerInterface):
     def setCredentials(self, phone, password):
-        self._layer.setCredentials(phone, password)
+        self._layer.setCredentials((phone, password))
 
     def getUsername(self, full = False):
         return self._layer.getUsername(full)
