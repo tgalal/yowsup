85f3e11aff67eb28163c53d4750cf44fde1bff22
Jorge Hortelano <softwaremagico@gmail.com>
2015-12-23 08:33:09 +0100

    Added code change as suggested in https://github.com/tgalal/yowsup/issues/683 to fix Bad Mac error.

--- yowsup/layers/protocol_messages/protocolentities/message_text.py
+++ yowsup/layers/protocol_messages/protocolentities/message_text.py
@@ -35,5 +35,10 @@ class TextMessageProtocolEntity(MessageProtocolEntity):
     def fromProtocolTreeNode(node):
         entity = MessageProtocolEntity.fromProtocolTreeNode(node)
         entity.__class__ = TextMessageProtocolEntity
-        entity.setBody(node.getChild("body").getData())
-        return entity
+	try:
+		entity.setBody(node.getChild("body").getData())
+		return entity
+	except Exception, e:
+		pass 
+	entity.setBody( None )
+	return entity
