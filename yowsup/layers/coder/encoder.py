7569a2acbcba32c49b580b748c6ee03db40c30c3
Tarek Galal <tare2.galal@gmail.com>
2016-01-10 14:54:04 +0100

    Encoder initial commit for wa1.6 t

--- yowsup/layers/coder/encoder.py
+++ yowsup/layers/coder/encoder.py
@@ -13,7 +13,7 @@ class WriteEncoder:
         data.append(87)
         data.append(65)
         data.append(1)
-        data.append(5)
+        data.append(6)
 
         streamOpenAttributes = {"to": domain, "resource": resource}
         self.writeListStart(len(streamOpenAttributes) * 2 + 1, data)
@@ -56,21 +56,30 @@ class WriteEncoder:
                 self.writeString(value, data);
 
 
-    def writeBytes(self, bytes, data):
-
-        length = len(bytes)
-        if length >= 256:
+    def writeBytes(self, bytes_, data, packed = False):
+        size = len(bytes_)
+        toWrite = bytes_
+        if size >= 0x100000:
+            data.append(254)
+            self.writeInt31(size, data)
+        elif size >= 0x100:
             data.append(253)
-            self.writeInt24(length, data)
+            self.writeInt20(size, data)
         else:
-            data.append(252)
-            self.writeInt8(length, data)
+            r = None
+            if packed:
+                if size < 128:
+                    r = self.tryPackAndWriteHeader(255, bytes_, data)
+                    if r is None:
+                        r = self.tryPackAndWriteHeader(251, bytes_, data)
 
-        for b in bytes:
-            if type(b) is int:
-                data.append(b)
+            if r is None:
+                data.append(252)
+                self.writeInt8(size, data)
             else:
-                data.append(ord(b))
+                toWrite = r
+
+        data.extend(toWrite)
 
     def writeInt8(self, v, data):
         data.append(v & 0xFF)
@@ -80,12 +89,21 @@ class WriteEncoder:
         data.append((v & 0xFF00) >> 8);
         data.append((v & 0xFF) >> 0);
 
+    def writeInt20(self, v, data):
+        data.append((0xF0000 & v) >> 16)
+        data.append((0xFF00 & v) >> 8)
+        data.append((v & 0xFF) >> 0)
 
     def writeInt24(self, v, data):
         data.append((v & 0xFF0000) >> 16)
         data.append((v & 0xFF00) >> 8)
         data.append((v & 0xFF) >> 0)
 
+    def writeInt31(self, v, data):
+        data.append((0x7F000000 & v) >> 24)
+        data.append((0xFF0000 & v) >> 16)
+        data.append((0xFF00 & v) >> 8)
+        data.append((v & 0xFF) >> 0)
 
     def writeListStart(self, i, data):
         if i == 0:
@@ -97,14 +115,14 @@ class WriteEncoder:
             data.append(249)
             self.writeInt16(i, data)
 
-    def writeToken(self, intValue, data):
-        if intValue < 245:
-            data.append(intValue)
-        elif intValue <=500:
-            data.append(254)
-            data.append(intValue - 245)
+    def writeToken(self, token, data):
+        if token <= 255 and token >=0:
+            data.append(token)
+        else:
+            raise ValueError("Invalid token: %s" % token)
 
-    def writeString(self, tag, data):
+
+    def writeString(self, tag, data, packed = False):
         tok = self.tokenDictionary.getIndex(tag)
         if tok:
             index, secondary = tok
@@ -123,7 +141,7 @@ class WriteEncoder:
                     user = tag[0:atIndex]
                     self.writeJid(user, server, data)
             except ValueError:
-                self.writeBytes(self.encodeString(tag), data)
+                self.writeBytes(self.encodeString(tag), data, packed)
 
     def encodeString(self, string):
         res = []
@@ -143,3 +161,52 @@ class WriteEncoder:
         else:
             self.writeToken(0, data)
         self.writeString(server, data)
+
+
+    def tryPackAndWriteHeader(self, v, headerData, data):
+        size = len(headerData)
+        if size >= 128:
+            return None
+
+        arr = [0] * int((size + 1) / 2)
+        for i in range(0, size):
+            packByte = self.packByte(v, headerData[i])
+            if packByte == -1:
+                arr = []
+                break
+            n2 = int(i / 2)
+            arr[n2] |= (packByte << 4 * (1 - i % 2))
+
+        if len(arr) > 0:
+            if size % 2 == 1:
+                arr[-1] |= 15 #0xF
+            data.append(v)
+            self.writeInt8(size %2 << 7 | len(arr), data)
+            return arr
+
+        return None
+
+
+
+    def packByte(self, v, n2):
+        if v == 251:
+            return self.packHex(n2)
+        if v == 255:
+            return self.packNibble(n2)
+        return -1
+
+    def packHex(self, n):
+        if n in range(48, 58):
+            return n - 48
+        if n in range(65, 71):
+            return 10 + (n - 65)
+        return -1
+
+    def packNibble(self, n):
+        if n in (45, 46):
+            return 10 + (n - 45)
+
+        if n in range(48, 58):
+            return n - 48
+
+        return -1
