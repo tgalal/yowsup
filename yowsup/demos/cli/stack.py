b824e49fbaaa8b55998d1d4aad110d136c436dbd
Tarek Galal <tare2.galal@gmail.com>
2016-04-09 16:15:23 +0200

    Split up axolotl into 3 layers

--- yowsup/demos/cli/stack.py
+++ yowsup/demos/cli/stack.py
@@ -3,7 +3,7 @@ from .layer import YowsupCliLayer
 from yowsup.layers.auth import AuthError
 from yowsup.layers import YowLayerEvent
 from yowsup.layers.auth import YowAuthenticationProtocolLayer
-from yowsup.layers.axolotl.layer import YowAxolotlLayer
+from yowsup.layers.axolotl import AxolotlReceivelayer
 import sys
 
 class YowsupCliStack(object):
@@ -17,7 +17,7 @@ class YowsupCliStack(object):
 
         # self.stack.setCredentials(credentials)
         self.stack.setCredentials(credentials)
-        self.stack.setProp(YowAxolotlLayer.PROP_IDENTITY_AUTOTRUST, True)
+        self.stack.setProp(AxolotlReceivelayer.PROP_IDENTITY_AUTOTRUST, True)
 
     def start(self):
         print("Yowsup Cli client\n==================\nType /help for available commands\n")
