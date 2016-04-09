f2e18dbb32d1ff50bc520eab4b97da21c3b78523
Tarek Galal <tare2.galal@gmail.com>
2016-04-09 16:15:23 +0200

    Split up axolotl into 3 layers

--- yowsup/layers/axolotl/__init__.py
+++ yowsup/layers/axolotl/__init__.py
@@ -1,1 +1,4 @@
-from .layer import YowAxolotlLayer
+# from .layer import YowAxolotlLayer
+from .layer_send import AxolotlSendLayer
+from .layer_control import AxolotlControlLayer
+from .layer_receive import AxolotlReceivelayer
