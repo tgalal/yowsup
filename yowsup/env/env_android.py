ae440780894f97a0486da9d3c52e0d6a0a8bdb81
GAURAV SINGH <Gaurav.ssnl@gmail.com>
2017-06-18 22:09:19 +0530

    Update env_android.py
     _VERSION = "2.17.221"

--- yowsup/env/env_android.py
+++ yowsup/env/env_android.py
@@ -17,7 +17,7 @@ class AndroidYowsupEnv(YowsupEnv):
         "YHNtYoIvt5R3X6YZylbPftF/8ayWTALBgcqhkjOOAQDBQADLwAwLAIUAKYCp0d6z4QQdyN74JDfQ2WCyi8CFDUM4CaNB+ceVXd" \
         "KtOrNTQcc0e+t"
 
-    _MD5_CLASSES = "1naz8gL5pIYWbtaOZ3207g=="
+    _MD5_CLASSES = "e4qMAO95WWMHgUFWwQtptw=="
     _KEY = "eQV5aq/Cg63Gsq1sshN9T3gh+UUp0wIw0xgHYT1bnCjEqOJQKCRrWxdAe2yvsDeCJL+Y4G3PRD2HUF7oUgiGo8vGlNJOaux26k+A2F3hj8A="
 
     _VERSION = "2.17.221"
