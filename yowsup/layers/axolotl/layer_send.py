3df9f169f3ce8a77dacded619a6bd1ef7cdaf7a6
Felipe Machado <fmachado.dev@gmail.com>
2017-09-24 14:53:27 -0300

    get keys for receipient id if there isn`t a session before send ecrypted media

--- yowsup/layers/axolotl/layer_send.py
+++ yowsup/layers/axolotl/layer_send.py
@@ -67,6 +67,8 @@ class AxolotlSendLayer(AxolotlBaseLayer):
             else:
                 messageData = self.serializeToProtobuf(node)
                 # print (messageData)
+                if not self.store.containsSession(recipient_id, 1):
+                    self.getKeysFor([node["to"]], lambda successJids, b: self.sendToContact(node) if len(successJids) == 1 else self.toLower(node), lambda: self.toLower(node))
                 if messageData:
                     sessionCipher = self.getSessionCipher(recipient_id)
                     messageData = messageData.SerializeToString() + self.getPadding()
