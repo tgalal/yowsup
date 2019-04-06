723274ef6d6a5e46939109b91d4426f9aee31e6c
Tarek Galal <tare2.galal@gmail.com>
2019-04-06 11:34:48 +0200

    [chore] update android env to 2.19.51

--- yowsup/env/env_android.py
+++ yowsup/env/env_android.py
@@ -17,10 +17,10 @@ class AndroidYowsupEnv(YowsupEnv):
         "YHNtYoIvt5R3X6YZylbPftF/8ayWTALBgcqhkjOOAQDBQADLwAwLAIUAKYCp0d6z4QQdyN74JDfQ2WCyi8CFDUM4CaNB+ceVXd" \
         "KtOrNTQcc0e+t"
 
-    _MD5_CLASSES = "JfoS1T2x3koJVaLtqWRpAA=="
+    _MD5_CLASSES = "bdZVQh58MKZaaeiYnh7wkw=="
     _KEY = "eQV5aq/Cg63Gsq1sshN9T3gh+UUp0wIw0xgHYT1bnCjEqOJQKCRrWxdAe2yvsDeCJL+Y4G3PRD2HUF7oUgiGo8vGlNJOaux26k+A2F3hj8A="
 
-    _VERSION = "2.18.379"
+    _VERSION = "2.19.51"
     _OS_NAME = "Android"
     _OS_VERSION = "4.3"
     _DEVICE_NAME = "endeavoru"
