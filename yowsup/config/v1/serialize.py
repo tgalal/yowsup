9e6464886c3cb1a5310491ff4f1be877f74da4e1
Tarek Galal <tare2.galal@gmail.com>
2019-04-08 00:47:35 +0200

    [feat] transform id, expid, client_static_keypair
    ConfigSerializer now base64 encodes client_static_keypair, id and expid on
    serialize, and decodes on deserialize

--- yowsup/config/v1/serialize.py
+++ yowsup/config/v1/serialize.py
@@ -3,6 +3,10 @@ from yowsup.config.transforms.filter import FilterTransform
 from yowsup.config.transforms.meta import MetaPropsTransform
 from yowsup.config.transforms.map import MapTransform
 from yowsup.config.transforms.config_dict import ConfigDictTransform
+from yowsup.config.transforms.props import PropsTransform
+
+from noisewa.structs.keypair import KeyPair
+import base64
 
 
 class ConfigSerialize(serialize.ConfigSerialize):
@@ -15,6 +19,18 @@ class ConfigSerialize(serialize.ConfigSerialize):
                     reverse_filter=lambda key, val: key != "version"
                 ),
                 MapTransform(transform_map=lambda key, val: (key[1:], val)),
+                PropsTransform(
+                    transform_map={
+                        "client_static_keypair": lambda key, val: (key, base64.b64encode(val.private.data + val.public.data).decode()),
+                        "id": lambda key, val: (key, base64.b64encode(val).decode()),
+                        "expid": lambda key, val: (key, base64.b64encode(val).decode())
+                    },
+                    reverse_map={
+                        "client_static_keypair": lambda key, val: (key, KeyPair.from_bytes(base64.b64decode(val))),
+                        "id": lambda key, val: (key, base64.b64decode(val)),
+                        "expid": lambda key, val: (key, base64.b64decode(val))
+                    }
+                ),
                 MetaPropsTransform(meta_props=("version", )),
             )
         )
