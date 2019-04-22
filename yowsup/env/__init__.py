79c38d35454ff6f2a23e90c9f9ad24d4fe3d27ec
Tarek Galal <tare2.galal@gmail.com>
2019-04-22 13:58:28 +0200

    [feat] remove s40 env
    Set android as default env

--- yowsup/env/__init__.py
+++ yowsup/env/__init__.py
@@ -1,3 +1,2 @@
 from .env import YowsupEnv
 from .env_android import AndroidYowsupEnv
-from .env_s40 import S40YowsupEnv
--- yowsup/env/test_env_s40.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from yowsup.env import S40YowsupEnv
-class S40YowsupEnvTest(unittest.TestCase):
-    def test_tokengen(self):
-        phone = "1234567"
-        S40YowsupEnv._TOKEN_STRING  = "PdA2DJyKoUrwLw1Bg6EIhzh502dF9noR9uFCllGk1425519315543{phone}"
-        env = S40YowsupEnv()
-        token = env.getToken(phone)
-        self.assertEqual(token, 'e84e1f1477704159efd46f6f0781dbde')
