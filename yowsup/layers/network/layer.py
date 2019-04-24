6b1093faa9bf25adbd4fbe44608ea42991726bb7
Tarek Galal <tare2.galal@gmail.com>
2019-04-24 22:34:47 +0200

    [feat] allow selecting a connection dispatcher
    Network layer allows configuring a preferred connection
    dispatcher using the layer property defined by constant
    YowNetworkLayer.PROP_DISPATCHER, where the value is
    currently can be YowNetworkLayer.DISPATCHER_SOCKET
    or YowNetworkLayer.DISPATCHER_ASYNCORE

--- yowsup/layers/network/layer.py
+++ yowsup/layers/network/layer.py
@@ -1,7 +1,9 @@
 from yowsup.layers import YowLayer, YowLayerEvent, EventCallback
 from yowsup.layers.network.layer_interface import YowNetworkLayerInterface
 from yowsup.layers.network.dispatcher.dispatcher import ConnectionCallbacks
+from yowsup.layers.network.dispatcher.dispatcher import YowConnectionDispatcher
 from yowsup.layers.network.dispatcher.dispatcher_socket import SocketConnectionDispatcher
+from yowsup.layers.network.dispatcher.dispatcher_asyncore import AsyncoreConnectionDispatcher
 import logging
 logger = logging.getLogger(__name__)
 
@@ -19,21 +21,35 @@ class YowNetworkLayer(YowLayer, ConnectionCallbacks):
 
     PROP_ENDPOINT               = "org.openwhatsapp.yowsup.prop.endpoint"
     PROP_NET_READSIZE           = "org.openwhatsapp.yowsup.prop.net.readSize"
+    PROP_DISPATCHER             = "org.openwhatsapp.yowsup.prop.net.dispatcher"
 
     STATE_DISCONNECTED          = 0
     STATE_CONNECTING            = 1
     STATE_CONNECTED             = 2
     STATE_DISCONNECTING         = 3
 
+    DISPATCHER_SOCKET = 0
+    DISPATCHER_ASYNCORE = 1
+    DISPATCHER_DEFAULT = DISPATCHER_SOCKET
+
     def __init__(self):
         self.state = self.__class__.STATE_DISCONNECTED
         YowLayer.__init__(self)
         ConnectionCallbacks.__init__(self)
         self.interface = YowNetworkLayerInterface(self)
-        self.serverConnection = SocketConnectionDispatcher(self)
+        self.connected = False
+        self._dispatcher = None  # type: YowConnectionDispatcher
+
+    def __create_dispatcher(self, dispatcher_type):
+        if dispatcher_type == self.DISPATCHER_ASYNCORE:
+            logger.debug("Created asyncore dispatcher")
+            return AsyncoreConnectionDispatcher(self)
+        else:
+            logger.debug("Created socket dispatcher")
+            return SocketConnectionDispatcher(self)
 
     def onConnected(self):
-        logger.debug("CONNECTED!!")
+        logger.debug("Connected")
         self.state = self.__class__.STATE_CONNECTED
         self.connected = True
         self.emitEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECTED))
@@ -62,21 +78,22 @@ class YowNetworkLayer(YowLayer, ConnectionCallbacks):
         return True
 
     def createConnection(self):
+        self._dispatcher = self.__create_dispatcher(self.getProp(self.PROP_DISPATCHER, self.DISPATCHER_DEFAULT))
         self.state = self.__class__.STATE_CONNECTING
         endpoint = self.getProp(self.__class__.PROP_ENDPOINT)
         logger.info("Connecting to %s:%s" % endpoint)
-        self.serverConnection.connect(endpoint)
+        self._dispatcher.connect(endpoint)
 
     def destroyConnection(self, reason = None):
         self.state = self.__class__.STATE_DISCONNECTING
-        self.serverConnection.disconnect()
+        self._dispatcher.disconnect()
 
     def getStatus(self):
         return self.connected
 
     def send(self, data):
         if self.connected:
-            self.serverConnection.sendData(data)
+            self._dispatcher.sendData(data)
 
     def onRecvData(self, data):
         self.receive(data)
