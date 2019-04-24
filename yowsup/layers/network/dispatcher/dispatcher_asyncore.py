301c39e847c16d6b3ffb793ef0ffb6773f755e25
Tarek Galal <tare2.galal@gmail.com>
2019-04-24 18:33:39 +0200

    [feat] complete Asyncore dispatcher impl
    The dispatcher was relying on the super class's "connected"
    instance variable to determine connectivity state. This
    variable update after the handle_connect callback completes
    causing data sent in that callback to be ignored.
    
    Removed threading as it's not needed with asyncore
    Fixed error handling

--- yowsup/layers/network/dispatcher/dispatcher_asyncore.py
+++ yowsup/layers/network/dispatcher/dispatcher_asyncore.py
@@ -1,50 +1,52 @@
 from yowsup.layers.network.dispatcher.dispatcher import YowConnectionDispatcher
 import asyncore
 import logging
-import threading
 import socket
+import traceback
 
 logger = logging.getLogger(__name__)
 
-'''
-    WIP, not finished and 
-    doesn't seem to be working properly, need to check why
-'''
 
 class AsyncoreConnectionDispatcher(YowConnectionDispatcher, asyncore.dispatcher_with_send):
     def __init__(self, connectionCallbacks):
         super(AsyncoreConnectionDispatcher, self).__init__(connectionCallbacks)
         asyncore.dispatcher_with_send.__init__(self)
+        self._connected = False
 
     def sendData(self, data):
-        if self.connected:
+        if self._connected:
             self.out_buffer = self.out_buffer + data
             self.initiate_send()
+        else:
+            logger.warn("Attempted to send %d bytes while still not connected" % len(data))
 
     def connect(self, host):
-        threading.Thread(target=self.__loop, args=(host,)).start()
+        logger.debug("connect(%s)" % str(host))
+        self.connectionCallbacks.onConnecting()
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        asyncore.dispatcher_with_send.connect(self, host)
+        asyncore.loop(timeout=1)
 
     def handle_connect(self):
-        if not self.connected:
+        logger.debug("handle_connect")
+        if not self._connected:
+            self._connected = True
             self.connectionCallbacks.onConnected()
 
     def handle_close(self):
-        self.connectionCallbacks.onDisonnected()
+        logger.debug("handle_close")
+        self.close()
+        self._connected = False
+        self.connectionCallbacks.onDisconnected()
+
+    def handle_error(self):
+        logger.error(traceback.format_exc())
+        self.handle_close()
 
     def handle_read(self):
         data = self.recv(1024)
         self.connectionCallbacks.onRecvData(data)
 
-    def handle_error(self):
-        pass
-
-    def __loop(self, host):
-        self.connectionCallbacks.onConnecting()
-        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-        asyncore.dispatcher_with_send.connect(self, host)
-        logger.debug("Entering loop")
-        asyncore.loop()
-        logger.debug("Exited loop")
-
     def disconnect(self):
-        self.close()
+        logger.debug("disconnect")
+        self.handle_close()
