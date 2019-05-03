4069eb8d18aca8ca278aa2a44eb2e722f67cd9ba
Tarek Galal <tare2.galal@gmail.com>
2019-05-03 12:20:23 +0200

    [doc] change vcard to contact in echoclient demo

--- yowsup/demos/echoclient/layer.py
+++ yowsup/demos/echoclient/layer.py
@@ -31,5 +31,5 @@ class EchoLayer(YowInterfaceLayer):
         elif messageProtocolEntity.media_type == "location":
             print("Echoing location (%s, %s) to %s" % (messageProtocolEntity.getLatitude(), messageProtocolEntity.getLongitude(), messageProtocolEntity.getFrom(False)))
 
-        elif messageProtocolEntity.media_type == "vcard":
-            print("Echoing vcard (%s, %s) to %s" % (messageProtocolEntity.getName(), messageProtocolEntity.getCardData(), messageProtocolEntity.getFrom(False)))
+        elif messageProtocolEntity.media_type == "contact":
+            print("Echoing contact (%s, %s) to %s" % (messageProtocolEntity.getName(), messageProtocolEntity.getCardData(), messageProtocolEntity.getFrom(False)))
