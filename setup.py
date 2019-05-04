c6cf471a137581db93835ca98644e5dfd5a811a6
Tarek Galal <tare2.galal@gmail.com>
2019-05-04 18:54:18 +0200

    [meta] set consonance==0.1.2, protobuf>=3.6.0
    Refs #2694 TypeError: __new__() got an unexpected keyword
    argument 'serialized_options'

--- setup.py
+++ setup.py
@@ -5,11 +5,10 @@ import yowsup
 import platform
 import sys
 
-deps = ['consonance==0.1.1', 'argparse', 'python-axolotl>=0.1.39', 'six==1.10', 'appdirs']
+deps = ['consonance==0.1.2', 'argparse', 'python-axolotl>=0.1.39', 'six==1.10', 'appdirs', 'protobuf>=3.6.0']
 
 if sys.version_info < (2, 7):
     deps.append('importlib')
-    deps.append('protobuf==3.4.0')
 
 if platform.system().lower() == "windows":
     deps.append('pyreadline')
