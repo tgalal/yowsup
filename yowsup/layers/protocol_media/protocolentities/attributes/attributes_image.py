e791f191287d7bbb46c015122aae936831e679c4
Tarek Galal <tare2.galal@gmail.com>
2019-05-06 20:37:30 +0200

    [feat] update ImageAttributes.from_filepath
    Accept dimensions in arguments
    Try figure out dimensions if not passed

--- yowsup/layers/protocol_media/protocolentities/attributes/attributes_image.py
+++ yowsup/layers/protocol_media/protocolentities/attributes/attributes_image.py
@@ -10,11 +10,12 @@ class ImageAttributes(object):
         self.jpeg_thumbnail = jpeg_thumbnail
 
     @staticmethod
-    def from_filepath(filepath, caption=None, jpeg_thumbnail=None):
+    def from_filepath(filepath, dimensions=None, caption=None, jpeg_thumbnail=None):
         assert os.path.exists(filepath)
         if not jpeg_thumbnail:
-            jpeg_thumbnail = ImageTools.generatePreviewFromImage(file)
-        width, height = ImageTools.getImageDimensions(filepath)
-        assert width and height, "Could not determine image dimensions"
+            jpeg_thumbnail = ImageTools.generatePreviewFromImage(filepath)
+        dimensions = dimensions or ImageTools.getImageDimensions(filepath)
+        width, height = dimensions if dimensions else (None, None)
+        assert width and height, "Could not determine image dimensions, install pillow or pass dimensions"
 
         return ImageAttributes(width, height, caption, jpeg_thumbnail)
