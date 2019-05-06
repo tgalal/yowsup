146b0b65098ad3ba98ffaae79f54f4ddc87dae31
Tarek Galal <tare2.galal@gmail.com>
2019-05-06 20:32:46 +0200

    [fix] fix props of image downloadable
    Fixed typo in constructor arguments
    jpegThumbnail -> jpeg_thumbnail
    Fixed props referencing wrong namespace under proto

--- yowsup/layers/protocol_media/protocolentities/message_media_downloadable_image.py
+++ yowsup/layers/protocol_media/protocolentities/message_media_downloadable_image.py
@@ -1,15 +1,20 @@
 from .message_media_downloadable import DownloadableMediaMessageProtocolEntity
+from yowsup.layers.protocol_media.protocolentities.attributes.attributes_downloadablemedia \
+    import DownloadableMediaMessageAttributes
+from yowsup.layers.protocol_media.protocolentities.attributes.attributes_image import ImageAttributes
+from yowsup.layers.protocol_messages.protocolentities.attributes.attributes_message import MessageAttributes
 
 
 class ImageDownloadableMediaMessageProtocolEntity(DownloadableMediaMessageProtocolEntity):
-    def __init__(self, image_attrs, downloadablemdia_attrs, message_attrs):
+    def __init__(self, image_attrs, downloadablemedia_attrs, message_attrs):
+        # type: (ImageAttributes, DownloadableMediaMessageAttributes, MessageAttributes) -> None
         super(ImageDownloadableMediaMessageProtocolEntity, self).__init__(
-            "image", downloadablemdia_attrs, message_attrs
+            "image", downloadablemedia_attrs, message_attrs
         )
         self.width = image_attrs.width
         self.height = image_attrs.height
         self.caption = image_attrs.caption
-        self.jpeg_thumbnail = image_attrs.jpegThumbnail
+        self.jpeg_thumbnail = image_attrs.jpeg_thumbnail
 
     @property
     def proto(self):
@@ -17,32 +22,32 @@ class ImageDownloadableMediaMessageProtocolEntity(DownloadableMediaMessageProtoc
 
     @property
     def width(self):
-        return self.proto.image_message.width
+        return self.proto.width
 
     @width.setter
     def width(self, value):
-        self.proto.image_message.width = value
+        self.proto.width = value
 
     @property
     def height(self):
-        return self.proto.image_message.height
+        return self.proto.height
 
     @height.setter
     def height(self, value):
-        self.proto.image_message.height = value
+        self.proto.height = value
 
     @property
     def jpeg_thumbnail(self):
-        return self.proto.image_message.jpeg_thumbnail
+        return self.proto.jpeg_thumbnail
 
     @jpeg_thumbnail.setter
     def jpeg_thumbnail(self, value):
-        self.proto.image_message.jpeg_thumbnail = value
+        self.proto.jpeg_thumbnail = value if value is not None else b""
 
     @property
     def caption(self):
-        return self.proto.image_message.caption
+        return self.proto.caption
 
     @caption.setter
     def caption(self, value):
-        self.proto.image_message.caption = value
+        self.proto.caption = value if value is not None else ""
