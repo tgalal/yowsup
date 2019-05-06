8eb4e32fcceedecf46aa84afb37b4b578ff7ebaa
Tarek Galal <tare2.galal@gmail.com>
2019-05-06 20:40:38 +0200

    [refact] update DownloadableMediaMessageAttribtues
    Allow setting remote parameters (url, media_key) later (allow None)
    Reorder constructor arguments to allow default values for url and
    media_key
    Changed _length to _file_length, _sha256 to _file_sha256
    Implemented __str__
    Added getters for all properties

--- yowsup/layers/protocol_media/protocolentities/attributes/attributes_downloadablemedia.py
+++ yowsup/layers/protocol_media/protocolentities/attributes/attributes_downloadablemedia.py
@@ -2,10 +2,37 @@ from yowsup.layers.protocol_media.protocolentities.attributes.attributes_media i
 
 
 class DownloadableMediaMessageAttributes(MediaAttributes):
-    def __init__(self, url, mimetype, length, sha256, media_key, context_info=None):
+    def __init__(self, mimetype, file_length, file_sha256, url=None, media_key=None, context_info=None):
         super(DownloadableMediaMessageAttributes, self).__init__(context_info)
-        self._url = url
         self._mimetype = mimetype
-        self._length = length
-        self._sha256 = sha256
+        self._file_length = file_length
+        self._file_sha256 = file_sha256
+        self._url = url
         self._media_key = media_key
+
+    def __str__(self):
+        return "[url=%s, mimetype=%s, file_length=%d, file_sha256=%s, media_key=%s]" % (
+            self.url,self.mimetype, self.file_length, base64.b64encode(self.file_sha256),
+            base64.b64encode(self.media_key)
+        )
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def mimetype(self):
+        return self._mimetype
+
+    @property
+    def file_length(self):
+        return self._file_length
+
+    @property
+    def file_sha256(self):
+        return self._file_sha256
+
+    @property
+    def media_key(self):
+        return self._media_key
+
