7e3115b9871236916e65c42734e3ea538d5c76db
Tarek Galal <tare2.galal@gmail.com>
2019-05-06 20:42:46 +0200

    [feat] add from_file to DownloadableMediaAttributes

--- yowsup/layers/protocol_media/protocolentities/attributes/attributes_downloadablemedia.py
+++ yowsup/layers/protocol_media/protocolentities/attributes/attributes_downloadablemedia.py
@@ -1,4 +1,8 @@
 from yowsup.layers.protocol_media.protocolentities.attributes.attributes_media import MediaAttributes
+from yowsup.common.tools import MimeTools
+import base64
+import hashlib
+import os
 
 
 class DownloadableMediaMessageAttributes(MediaAttributes):
@@ -36,3 +40,16 @@ class DownloadableMediaMessageAttributes(MediaAttributes):
     def media_key(self):
         return self._media_key
 
+    @staticmethod
+    def from_file(
+            filepath, mimetype=None, file_length=None,
+            file_sha256=None, url=None, media_key=None, context_info=None
+    ):
+        mimetype = MimeTools.getMIME(filepath) if mimetype is None else mimetype
+        file_length = os.path.getsize(filepath) if file_length is None else file_length
+        if file_sha256 is None:
+            with open(filepath, 'rb') as f:
+                file_sha256 = hashlib.sha256(f.read()).digest()
+        return DownloadableMediaMessageAttributes(
+            mimetype, file_length, file_sha256, url, media_key, context_info
+        )
