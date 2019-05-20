6a3d20bac265d7be38f4fc716fa32c51e58e475a
Tarek Galal <tare2.galal@gmail.com>
2019-05-20 07:55:24 +0200

    [fix] update encoder's writeString

--- yowsup/layers/coder/encoder.py
+++ yowsup/layers/coder/encoder.py
@@ -117,9 +117,23 @@ class WriteEncoder:
         tok = self.tokenDictionary.getIndex(tag)
         if tok:
             index, secondary = tok
-            if secondary:
-                self.writeToken(236, data)
-            self.writeToken(index, data)
+            if not secondary:
+                self.writeToken(index, data)
+            else:
+                quotient = index // 256
+                if quotient == 0:
+                    double_byte_token = 236
+                elif quotient == 1:
+                    double_byte_token = 237
+                elif quotient == 2:
+                    double_byte_token = 238
+                elif quotient == 3:
+                    double_byte_token = 239
+                else:
+                    raise ValueError("Double byte dictionary token out of range")
+
+                self.writeToken(double_byte_token, data)
+                self.writeToken(index % 256, data)
         else:
             at = '@'.encode() if type(tag) == bytes else '@'
             try:
