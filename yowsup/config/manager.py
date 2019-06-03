500f576b7626e50337280576542c7812eb70856c
Tarek Galal <tare2.galal@gmail.com>
2019-06-03 11:31:09 +0200

    [feat] combine ConfigManager's load and load_path
    Calling ConfigManager's load will now attempt to load given parameter as
    path first and failover to interpreting it as profile name. This change
    makes loading configs simpler, as well as prevents error/warning logs
    from load_path which confused some users when path was not found, while
    in fact loading config from profile was still going to be attempted by
    the invoker.

--- yowsup/config/manager.py
+++ yowsup/config/manager.py
@@ -30,26 +30,45 @@ class ConfigManager(object):
         TYPE_JSON: DictJsonTransform
     }
 
-    def load(self, profile_name):
-        # type: (str) -> Config
+    def load(self, path_or_profile_name, profile_only=False):
+        # type: (str, bool) -> Config
+        """
+        Will first try to interpret path_or_profile_name as direct path to a config file and load from there. If
+        this fails will interpret it as profile name and load from profile dir.
+        :param path_or_profile_name:
+        :param profile_only
+        :return Config instance, or None if no config could be found
+        """
+        logger.debug("load(path_or_profile_name=%s, profile_only=%s)" % (path_or_profile_name, profile_only))
 
-        config_dir = StorageTools.getStorageForProfile(profile_name)
-        logger.debug("Detecting config for profile=%s, dir=%s" % (profile_name, config_dir))
         exhausted = []
-        for ftype in self.MAP_EXT:
-            if len(ftype):
-                fname = (self.NAME_FILE_CONFIG + "." + ftype)
-            else:
-                fname = self.NAME_FILE_CONFIG
+        if not profile_only:
+            config = self._load_path(path_or_profile_name)
+        else:
+            config = None
+        if config is not None:
+            return config
+        else:
+            logger.debug("path_or_profile_name is not a path, using it as profile name")
+            if not profile_only:
+                exhausted.append(path_or_profile_name)
+            profile_name = path_or_profile_name
+            config_dir = StorageTools.getStorageForProfile(profile_name)
+            logger.debug("Detecting config for profile=%s, dir=%s" % (profile_name, config_dir))
+            for ftype in self.MAP_EXT:
+                if len(ftype):
+                    fname = (self.NAME_FILE_CONFIG + "." + ftype)
+                else:
+                    fname = self.NAME_FILE_CONFIG
 
-            fpath = os.path.join(config_dir, fname)
-            logger.debug("Trying %s" % fpath)
-            if os.path.isfile(fpath):
-                return self.load_path(fpath)
+                fpath = os.path.join(config_dir, fname)
+                logger.debug("Trying %s" % fpath)
+                if os.path.isfile(fpath):
+                    return self._load_path(fpath)
 
-            exhausted.append(fpath)
+                exhausted.append(fpath)
 
-        logger.error("Could not find a config for profile=%s, paths checked: %s" % (profile_name, ":".join(exhausted)))
+            logger.error("Could not find a config for profile=%s, paths checked: %s" % (profile_name, ":".join(exhausted)))
 
     def _type_to_str(self, type):
         """
@@ -62,14 +81,14 @@ class ConfigManager(object):
             if key == type:
                 return val
 
-    def load_path(self, path):
+    def _load_path(self, path):
         """
         :param path:
         :type path:
         :return:
         :rtype:
         """
-        logger.debug("load_path(path=%s)" % path)
+        logger.debug("_load_path(path=%s)" % path)
         if os.path.isfile(path):
             configtype = self.guess_type(path)
             logger.debug("Detected config type: %s" % self._type_to_str(configtype))
@@ -82,7 +101,7 @@ class ConfigManager(object):
             else:
                 raise ValueError("Unsupported config type")
         else:
-            logger.warn("load_path couldn't find the path: %s" % path)
+            logger.debug("_load_path couldn't find the path: %s" % path)
 
     def load_data(self, datadict):
         logger.debug("Loading config")
