2adc067f306d9e7d8b634f66e96c52d80a42e1ff
AL-Kateb <obada.kateb@gmail.com>
2019-09-05 14:12:30 +0300

    Fixed for version 2.19.244

--- yowsup/env/env_android.py
+++ yowsup/env/env_android.py
@@ -17,10 +17,10 @@ class AndroidYowsupEnv(YowsupEnv):
         "YHNtYoIvt5R3X6YZylbPftF/8ayWTALBgcqhkjOOAQDBQADLwAwLAIUAKYCp0d6z4QQdyN74JDfQ2WCyi8CFDUM4CaNB+ceVXd" \
         "KtOrNTQcc0e+t"
 
-    _MD5_CLASSES = "bdZVQh58MKZaaeiYnh7wkw=="
+    _MD5_CLASSES = "/QhoCBMppKpKQumhTC8kcQ=="
     _KEY = "eQV5aq/Cg63Gsq1sshN9T3gh+UUp0wIw0xgHYT1bnCjEqOJQKCRrWxdAe2yvsDeCJL+Y4G3PRD2HUF7oUgiGo8vGlNJOaux26k+A2F3hj8A="
 
-    _VERSION = "2.19.51"
+    _VERSION = "2.19.244"
     _OS_NAME = "Android"
     _OS_VERSION = "8.0.0"
     _DEVICE_NAME = "star2lte"
