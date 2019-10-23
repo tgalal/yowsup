710847f975f1894295d903580fd7964c28d6e86a
Lashmanov <lashmanov@group-ib.ru>
2019-10-23 20:03:43 +0300

    Add possibility to provide string client_static_keypair (not only bytes)

--- yowsup/layers/noise/layer.py
+++ yowsup/layers/noise/layer.py
@@ -18,6 +18,7 @@ from consonance.structs.keypair import KeyPair
 
 import threading
 import logging
+import base64
 
 
 logger = logging.getLogger(__name__)
@@ -40,7 +41,8 @@ class YowNoiseLayer(YowLayer):
         )  # type: WANoiseProtocol
 
         self._handshake_worker = None
-        self._stream = BlockingQueueSegmentedStream()  # type: BlockingQueueSegmentedStream
+        # type: BlockingQueueSegmentedStream
+        self._stream = BlockingQueueSegmentedStream()
         self._read_buffer = bytearray()
         self._flush_lock = threading.Lock()
         self._incoming_segments_queue = Queue.Queue()
@@ -64,7 +66,8 @@ class YowNoiseLayer(YowLayer):
         username = int(self._profile.username)
 
         if local_static is None:
-            logger.error("client_static_keypair is not defined in specified config, disconnecting")
+            logger.error(
+                "client_static_keypair is not defined in specified config, disconnecting")
             self.broadcastEvent(
                 YowLayerEvent(
                     YowNetworkLayer.EVENT_STATE_DISCONNECT,
@@ -72,8 +75,12 @@ class YowNoiseLayer(YowLayer):
                 )
             )
         else:
-            if type(local_static) is bytes:
+            _type = type(local_static)
+            if _type is bytes:
                 local_static = KeyPair.from_bytes(local_static)
+            elif _type is str:
+                local_static = KeyPair.from_bytes(
+                    base64.b64decode(local_static))
             assert type(local_static) is KeyPair, type(local_static)
             passive = event.getArg('passive')
 
@@ -112,7 +119,8 @@ class YowNoiseLayer(YowLayer):
                 short_connect=True
             )
             if not self._in_handshake():
-                logger.debug("Performing handshake [username= %d, passive=%s]" % (username, passive) )
+                logger.debug(
+                    "Performing handshake [username= %d, passive=%s]" % (username, passive))
                 self._handshake_worker = WANoiseProtocolHandshakeWorker(
                     self._wa_noiseprotocol, self._stream, client_config, local_static, remote_static,
                     self.on_handshake_finished
@@ -124,12 +132,14 @@ class YowNoiseLayer(YowLayer):
     def on_handshake_finished(self, e=None):
         # type: (Exception) -> None
         if e is not None:
-            self.emitEvent(YowLayerEvent(self.EVENT_HANDSHAKE_FAILED, reason=e))
-            data=WriteEncoder(TokenDictionary()).protocolTreeNodeToBytes(
+            self.emitEvent(YowLayerEvent(
+                self.EVENT_HANDSHAKE_FAILED, reason=e))
+            data = WriteEncoder(TokenDictionary()).protocolTreeNodeToBytes(
                 ProtocolTreeNode("failure", {"reason": str(e)})
             )
             self.toUpper(data)
-            logger.error("An error occurred during handshake, try login again.")
+            logger.error(
+                "An error occurred during handshake, try login again.")
 
     def _in_handshake(self):
         """
@@ -151,7 +161,8 @@ class YowNoiseLayer(YowLayer):
         if event == BlockingQueueSegmentedStream.EVENT_WRITE:
             self.toLower(self._stream.get_write_segment())
         elif event == BlockingQueueSegmentedStream.EVENT_READ:
-            self._stream.put_read_segment(self._incoming_segments_queue.get(block=True))
+            self._stream.put_read_segment(
+                self._incoming_segments_queue.get(block=True))
 
     def send(self, data):
         """
