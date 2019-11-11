a986af05ca5caf0be1565909008b598de20119de
RomaLash <lash684@gmail.com>
2019-11-11 17:46:12 +0300

    Update version
    Revert changes in version (cause of different between repository and Pypi versions)

--- yowsup/env/env_android.py
+++ yowsup/env/env_android.py
@@ -25,10 +25,10 @@ class AndroidYowsupEnv(YowsupEnv):
         3. Replace the appropriate values (_MD5_CLASSES and _VERSION)
     """
 
-    _MD5_CLASSES = "byLExGUNHi7RMfrg6wgCkQ=="
+    _MD5_CLASSES = "/QhoCBMppKpKQumhTC8kcQ=="
     _KEY = "eQV5aq/Cg63Gsq1sshN9T3gh+UUp0wIw0xgHYT1bnCjEqOJQKCRrWxdAe2yvsDeCJL+Y4G3PRD2HUF7oUgiGo8vGlNJOaux26k+A2F3hj8A="
 
-    _VERSION = "2.17.253"
+    _VERSION = "2.19.244"
     _OS_NAME = "Android"
     _OS_VERSION = "8.0.0"
     _DEVICE_NAME = "star2lte"
