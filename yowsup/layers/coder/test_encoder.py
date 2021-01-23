3f90ffa12423f00bebb8a25c0035cc2849801703
eldereng <eldereng@hotmail.com>
2021-01-23 14:37:53 -0300

    Whatsapp version 2.20.206.24 + @iacup pull request

--- yowsup/layers/coder/test_encoder.py
+++ yowsup/layers/coder/test_encoder.py
@@ -9,14 +9,14 @@ class EncoderTest(unittest.TestCase):
         self.encoder = WriteEncoder(TokenDictionary())
 
     def test_encode(self):
-        node = ProtocolTreeNode("message", {"form": "abc", "to":"xyz"}, [ProtocolTreeNode("media", {"width" : "123"}, data=b"123456")])
+        node = ProtocolTreeNode("message", {"from": "abc", "to":"xyz"}, [ProtocolTreeNode("media", {"width" : "123"}, data=b"123456")])
         result = self.encoder.protocolTreeNodeToBytes(node)
 
         self.assertTrue(result in (
-            [0, 248, 6, 95, 179, 252, 3, 120, 121, 122, 252, 4, 102, 111, 114, 109, 252, 3, 97, 98, 99, 248, 1, 248, 4, 93,
-             236, 104, 255, 130, 18, 63, 252, 6, 49, 50, 51, 52, 53, 54],
-            [0, 248, 6, 95, 252, 4, 102, 111, 114, 109, 252, 3, 97, 98, 99, 179, 252, 3, 120, 121, 122, 248, 1, 248, 4, 93,
-             236, 104, 255, 130, 18, 63, 252, 6, 49, 50, 51, 52, 53, 54]
+            [0, 248, 6, 9, 11, 252, 3, 120, 121, 122, 5, 252, 3, 97, 98, 99, 248, 1, 248, 4, 50, 238, 86, 255, 130, 18, 63,
+             252, 6, 49, 50, 51, 52, 53, 54],
+            [0, 248, 6, 9, 5, 252, 3, 97, 98, 99, 11, 252, 3, 120, 121, 122, 248, 1, 248, 4, 50, 238, 86, 255, 130, 18, 63,
+             252, 6, 49, 50, 51, 52, 53, 54]
             )
         )
 
