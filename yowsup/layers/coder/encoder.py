8a1561c5c5b72d392385ee0a405a8e5afb33b646
zzj <29055749+zjzh@users.noreply.github.com>
2022-03-08 10:05:13 +0800

    refactoring code with chain comparison
    refactoring code with chain comparison which is more pythonic, concise, readable and efficient

--- yowsup/layers/coder/encoder.py
+++ yowsup/layers/coder/encoder.py
@@ -107,7 +107,7 @@ class WriteEncoder:
             self.writeInt16(i, data)
 
     def writeToken(self, token, data):
-        if token <= 255 and token >=0:
+        if  255>= token >=0:
             data.append(token)
         else:
             raise ValueError("Invalid token: %s" % token)
