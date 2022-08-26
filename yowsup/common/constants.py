1b4574a0647c0032542d8faeb2c342c3f46248d6
gradol <gradol@gmail.com>
2022-08-26 22:23:50 +0300

    Updated ENDPOINTS

--- yowsup/common/constants.py
+++ yowsup/common/constants.py
@@ -1,22 +1,8 @@
 class YowConstants:
     DOMAIN       = "s.whatsapp.net"
     ENDPOINTS     = (
-        ("e1.whatsapp.net", 443),
-        ("e2.whatsapp.net", 443),
-        ("e3.whatsapp.net", 443),
-        ("e4.whatsapp.net", 443),
-        ("e5.whatsapp.net", 443),
-        ("e6.whatsapp.net", 443),
-        ("e7.whatsapp.net", 443),
-        ("e8.whatsapp.net", 443),
-        ("e9.whatsapp.net", 443),
-        ("e10.whatsapp.net", 443),
-        ("e11.whatsapp.net", 443),
-        ("e12.whatsapp.net", 443),
-        ("e13.whatsapp.net", 443),
-        ("e14.whatsapp.net", 443),
-        ("e15.whatsapp.net", 443),
-        ("e16.whatsapp.net", 443),
+        ("chat.cdn.whatsapp.net", 443),
+        ("g.whatsapp.net", 443),
         )
 
     WHATSAPP_SERVER = "s.whatsapp.net"
